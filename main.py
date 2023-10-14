42c5eb241e3f78a1686cdb1ba538109d8761b67e
Pedro Tavares <ptavaresh@gmail.com>
2023-10-14 16:40:51 -0600

    added multifiles upload and merge pdf endpoint

--- main.py
+++ main.py
@@ -1,15 +1,47 @@
 from PIL import Image
 import aiofiles
 from docx2pdf import convert
-
+from PyPDF2 import PdfMerger, PdfReader
 
 from fastapi import FastAPI, UploadFile, HTTPException
 from fastapi.responses import FileResponse
 
-app = FastAPI()
+app = FastAPI() 
 
 
-@app.post("/compress/uploadfile/")
+@app.post("/pdf/mergePdf/")
+async def merge_pdf_files(files: list[UploadFile]):
+    """
+    Merge PDF files:
+
+    - **file**: a valid application/pdf file 
+
+    """
+    allowed_types = ["application/pdf"]
+    #Create an instance of PdfFileMerger() class
+    merger = PdfMerger()
+    for i in files:
+        if i.content_type not in allowed_types:
+            raise HTTPException(
+                status_code=415,
+                detail="El tipo de archivo no es válido. Tipos permitidos: {}".format(
+                    allowed_types
+                ),
+            )
+        async with aiofiles.open(i.filename, 'wb') as out_file:
+            content = await i.read()  # async read
+            await out_file.write(content)  # async write
+
+    #Iterate over the list of the file paths
+    for pdf_file in files:
+        #Append PDF files 
+        merger.append(pdf_file.filename)
+    #Write out the merged PDF file
+    merger.write("pdf_merged.pdf")
+    merger.close()
+    return FileResponse('pdf_merged.pdf')
+
+@app.post("/compress/uploadimage/")
 async def create_upload_file(file: UploadFile):
     """
     Compress a JPEG image:
@@ -42,7 +74,6 @@ async def create_file(file: UploadFile):
 
     """
     allowed_types = ["application/vnd.openxmlformats-officedocument.wordprocessingml.document"]
-    print(file.content_type)
     if file.content_type not in allowed_types:
         raise HTTPException(
             status_code=415,
@@ -84,3 +115,5 @@ async def create_file(file: UploadFile):
         input_img.save('compressed.pdf', save_all=True)
 
     return FileResponse('compressed.pdf')
+
+#python -m uvicorn main:app --reload
